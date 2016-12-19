9ae9ae73a71a275a9acdef088a9c7946e36cb888
Cooper Legeza <cooperlegeza@gmail.com>
2016-12-19 08:37:31 -0500

    Initial commit

---
+++ src/main/java/com/manifestcorp/dicekata/App.java
@@ -0,0 +1,13 @@
+package com.manifestcorp.dicekata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
